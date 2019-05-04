00579c75bd5ce535f07522ca9c1b749c4d9f14bd
endigo <tsogt.otgonbaatar@gmail.com>
2019-05-04 22:16:47 +0800

    pass options to android

--- android/src/main/java/io/endigo/plugins/pdfviewflutter/FlutterPDFView.java
+++ android/src/main/java/io/endigo/plugins/pdfviewflutter/FlutterPDFView.java
@@ -51,40 +51,74 @@ public class FlutterPDFView implements PlatformView, MethodCallHandler {
     @Override
     public void onMethodCall(MethodCall methodCall, Result result) {
         switch (methodCall.method) {
-            case "open":
-//                openFile(methodCall, result);
+            case "pageCount":
+                getPageCount(result);
                 break;
+            case "currentPage":
+                getCurrentPage(result);
+                break;
+            case "setPage":
+                setPage(methodCall, result);
+            case "updateSettings":
+                setPage(methodCall, result);
             default:
                 result.notImplemented();
         }
+
+        // total page count - pdfView.getPageCount();
+        // current page -  pdfView.getCurrentPage();
+//        pdfView.on
     }
 
-//    void openFile(MethodCall methodCall, Result result) {
-//        Map<String, Object> arguments = (Map<String, Object>) methodCall.arguments;
-//        String filePath = (String) arguments.get("filePath");
-//
-//        pdfView.fromFile(file)
-//            .enableSwipe(getBoolean(methodCall, "enableSwipe"))
-//            .swipeHorizontal(getBoolean(methodCall, "swipeHorizontal"))
-//            .password(getString(methodCall,"password"))
-//            .nightMode(getBoolean(methodCall,"nightMode"))
-//            .autoSpacing(getBoolean(methodCall,"autoSpacing"))
-//            .pageFling(getBoolean(methodCall,"pageFling"))
-//            .pageSnap(getBoolean(methodCall,"pageSnap"))
-//            .enableDoubletap(true)
-//            .defaultPage(0)
-//            .load();
-//
-//        result.success(null);
-//    }
+    void getPageCount(Result result) {
+        result.success(pdfView.getPageCount());
+    }
+
+    void getCurrentPage(Result result) {
+        result.success(pdfView.getCurrentPage());
+    }
+
+    void setPage(MethodCall call, Result result) {
+        int page = (int)call.argument("page");
+        pdfView.jumpTo(page);
+        result.success(true);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void updateSettings(MethodCall methodCall, Result result) {
+        applySettings((Map<String, Object>) methodCall.arguments);
+        result.success(null);
+    }
+
+    private void applySettings(Map<String, Object> settings) {
+        for (String key : settings.keySet()) {
+            switch (key) {
+                case "enableSwipe":
+                    pdfView.setSwipeEnabled(getBoolean(settings, key));
+                    break;
+                case "nightMode":
+                    pdfView.setNightMode(getBoolean(settings, key));
+                    break;
+                case "pageFling":
+                    pdfView.setPageFling(getBoolean(settings, key));
+                    break;
+                case "pageSnap":
+                    pdfView.setPageSnap(getBoolean(settings, key));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown PDFView setting: " + key);
+            }
+        }
+    }
+
 
     @Override
     public void dispose() {
         methodChannel.setMethodCallHandler(null);
     }
 
-    Boolean getBoolean(Map<String, Object> params, String key) {
-        return params.containsKey(key) ? (Boolean) params.get(key): false;
+    boolean getBoolean(Map<String, Object> params, String key) {
+        return params.containsKey(key) ? (boolean) params.get(key): false;
     }
 
     String getString(Map<String, Object> params, String key) {
