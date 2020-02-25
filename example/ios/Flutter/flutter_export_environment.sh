327b886911881f4532592a6092357e989fd39f73
Arnaud Delubac <arnaud.delubac@interactiu.cat>
2020-02-25 08:14:42 +0100

    changing package name

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/arnaud/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/arnaud/dev/FlutterPlugin/flutter_pdfview/example"
+export "FLUTTER_APPLICATION_PATH=/Users/arnaud/dev/FlutterPlugin/modified/flutter_pdfview/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
