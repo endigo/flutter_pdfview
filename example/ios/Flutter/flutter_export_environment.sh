17474c3cbdd26a6990fa9d75d57802f87cac9557
endigo <tsogt.otgonbaatar@gmail.com>
2020-05-23 21:23:37 +0800

    update packages, set versions

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -5,6 +5,7 @@ export "FLUTTER_APPLICATION_PATH=/Users/macbookpro/Projects/dart/flutter_pdfview
 export "FLUTTER_TARGET=/Users/macbookpro/Projects/dart/flutter_pdfview/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/macbookpro/Projects/sdk/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
