032c5ba78524b8eb6c96bc3a8eb0b6c2394a9f5c
lcyper <lcyper@gmail.com>
2022-02-16 16:06:52 -0300

    add gesture detector on pdf view

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -191,61 +191,61 @@ class _PDFScreenState extends State<PDFScreen> with WidgetsBindingObserver {
           ),
         ],
       ),
-      body: Stack(
-        children: <Widget>[
-          PDFView(
-            filePath: widget.path,
-            enableSwipe: true,
-            swipeHorizontal: true,
-            autoSpacing: false,
-            pageFling: true,
-            pageSnap: true,
-            defaultPage: currentPage!,
-            fitPolicy: FitPolicy.BOTH,
-            preventLinkNavigation:
-                false, // if set to true the link is handled in flutter
-            onRender: (_pages) {
-              setState(() {
-                pages = _pages;
-                isReady = true;
-              });
-            },
-            onError: (error) {
-              setState(() {
-                errorMessage = error.toString();
-              });
-              print(error.toString());
-            },
-            onPageError: (page, error) {
-              setState(() {
-                errorMessage = '$page: ${error.toString()}';
-              });
-              print('$page: ${error.toString()}');
-            },
-            onViewCreated: (PDFViewController pdfViewController) {
-              _controller.complete(pdfViewController);
-            },
-            onLinkHandler: (String? uri) {
-              print('goto uri: $uri');
-            },
-            onPageChanged: (int? page, int? total) {
-              print('page change: $page/$total');
-              setState(() {
-                currentPage = page;
-              });
-            },
-          ),
-          errorMessage.isEmpty
-              ? !isReady
-                  ? Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : Container()
-              : Center(
-                  child: Text(errorMessage),
-                )
-        ],
+      body: GestureDetector(
+        onTap: () => print('tap'),
+        child: PDFView(
+          filePath: widget.path,
+          enableSwipe: true,
+          // swipeHorizontal: true,
+          autoSpacing: false,
+          pageFling: false,
+          pageSnap: false,
+          defaultPage: currentPage!,
+          fitPolicy: FitPolicy.BOTH,
+          preventLinkNavigation:
+              false, // if set to true the link is handled in flutter
+          onRender: (_pages) {
+            setState(() {
+              pages = _pages;
+              isReady = true;
+            });
+          },
+          onError: (error) {
+            setState(() {
+              errorMessage = error.toString();
+            });
+            print(error.toString());
+          },
+          onPageError: (page, error) {
+            setState(() {
+              errorMessage = '$page: ${error.toString()}';
+            });
+            print('$page: ${error.toString()}');
+          },
+          onViewCreated: (PDFViewController pdfViewController) {
+            _controller.complete(pdfViewController);
+          },
+          onLinkHandler: (String? uri) {
+            print('goto uri: $uri');
+          },
+          onPageChanged: (int? page, int? total) {
+            print('page change: $page/$total');
+            setState(() {
+              currentPage = page;
+            });
+          },
+        ),
       ),
+      // errorMessage.isEmpty
+      //     ? !isReady
+      //         ? Center(
+      //             child: CircularProgressIndicator(),
+      //           )
+      //         : Container()
+      //     : Center(
+      //         child: Text(errorMessage),
+      //       )
+
       floatingActionButton: FutureBuilder<PDFViewController>(
         future: _controller.future,
         builder: (context, AsyncSnapshot<PDFViewController> snapshot) {
