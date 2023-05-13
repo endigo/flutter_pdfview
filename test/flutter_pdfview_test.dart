225d2bcfd5964b6bd8f167e574dc741485d96a00
endigo <endigo.18@gmail.com>
2023-05-13 20:42:08 +0800

    use new api on test

--- test/flutter_pdfview_test.dart
+++ test/flutter_pdfview_test.dart
@@ -6,13 +6,15 @@ void main() {
   const MethodChannel channel = MethodChannel('flutter_pdfview');
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
       return '42';
     });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
